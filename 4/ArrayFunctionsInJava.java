d07ff66bda2b1bbe9424e3c8349d7523fb302e03
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-04-19 16:55:16 -0400

    add reverse array method

--- 4/ArrayFunctionsInJava.java
+++ 4/ArrayFunctionsInJava.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayFunctionsInJava {
     public static long sum_array(int arr[]){
         long res = 0;
@@ -8,11 +10,23 @@ public class ArrayFunctionsInJava {
         return res;
     }
 
+    public static int[] reverse_array(int arr[]){
+        int j = 0;
+        int[] res  = new int[arr.length];
+        for(int i = arr.length - 1; i >=0; i --){
+            res[j] = arr[i];
+            j++;
+        }
+        return res;
+    }
 
 
-    public static void main(){
-        int array[] = {101, 303, 505};
-        sum_array(array);
+    public static void main(String[] args){
+        int array[] = {101, 303, 404, 505, 707};
+        System.out.println("Sum of arr elements: " + sum_array(array));
+
+        System.out.println("Reverse of arr: " + Arrays.toString(reverse_array(array)));
+        
     }
     
 }
