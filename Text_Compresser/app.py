966ee53f2c189f6dee70f9ceaa29902343700740
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-04-21 18:03:05 -0400

    Use dictionary to map words, compress

--- Text_Compresser/app.py
+++ Text_Compresser/app.py
@@ -5,7 +5,17 @@ with open("input.txt") as file:
     words = re.findall(r"([a-zA-Z\-]+)", file.read())
     print(words)
 
+compressed_set = {}
+index = 1
 
+for word in set(words):  
+    compressed_set[word] = index
+    index += 1
+
+
+with open("output.sc", "w") as file:
+    for word in words: 
+        file.write(f"{compressed_set[word]} ")
 
 
 
