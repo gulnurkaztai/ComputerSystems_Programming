45ef7c2cd7281b0e5ec2efbbe2633c4d4a2721f5
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-04-20 23:38:29 -0400

    move_zeroes in python

--- 4/ArrayFunctionsInPython.py
+++ 4/ArrayFunctionsInPython.py
@@ -1,50 +1,65 @@
 class ArrayFunctionsInPython:
     @staticmethod
-    def sum_list(list):
+    def sum_arr(arr):
         res = 0
-        for el in list:
-            res+= el
+        for i in arr:
+            res+= i
         return res
     
     @staticmethod
-    def reverse_list(list):
+    def reverse_arr(arr):
         j = 0
-        res = [0]*len(list)
-        for i in range(len(list) -1, -1,-1):
-            res[j] = list[i]
+        res = [0]*len(arr)
+        for i in range(len(arr) -1, -1,-1):
+            res[j] = arr[i]
             j+=1
         return res
     
     @staticmethod
-    def is_polindrome(list_str):
-        end = len(list_str) - 1
-        for i in range(len(list_str) // 2):
-            if list_str[i] != list_str[end - i]:
+    def is_polindrome(arr_str):
+        end = len(arr_str) - 1
+        for i in range(len(arr_str) // 2):
+            if arr_str[i] != arr_str[end - i]:
                 return False
         return True
     
     @staticmethod
-    def remove_dups(list):
-        size = len(list)
+    def remove_dups(arr):
+        size = len(arr)
 
         for i in range(size - 1):
             for j in range(size - i - 1):
-                if list[j] > list[j+1]:
-                    list[j], list[j + 1] = list[j + 1], list[j]
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
         temp = []
         for i in range(size-1):
-            if list[i] != list[i + 1]:
-                temp.append(list[i])
+            if arr[i] != arr[i + 1]:
+                temp.append(arr[i])
         
-        temp.append(list[size - 1])
+        temp.append(arr[size - 1])
         return temp
+    
+    @staticmethod
+    def move_zeroes(arr):
+        j =0
+        for i in arr:
+            if i != 0:
+                arr[j] = i
+                j += 1
+
+        for i in range(j, len(arr)):
+                arr[i] = 0
+        return arr
+
 
 
     if __name__ == "__main__":
-        my_list = [101, 303, 404, 505, 707, 100, 100];
+        my_arr = [101, 303, 404, 505, 707, 100, 100];
+        my_other_arr = [1,5,0,0,7];
         my_str = ["hello", "world", "and", "world", "hello"]
-        print("Sum of arr elements: ", sum_list(my_list))
-        print("Reverse of arr: ", reverse_list(my_list))
+        print("Sum of arr elements: ", sum_arr(my_arr))
+        print("Reverse of arr: ", reverse_arr(my_arr))
         print("Is polindrome: ", is_polindrome(my_str))
-        print("Array after removing duplicates: ", remove_dups(my_list))
+        print("Array after removing duplicates: ", remove_dups(my_arr))
+        print("Array with zeroes at the end: ", move_zeroes(my_other_arr))
