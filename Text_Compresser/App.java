178a627b172470878e62fca87dfb2221917e265b
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-04-27 23:37:11 -0400

    Reading file and outputting the same for now

--- Text_Compresser/App.java
+++ Text_Compresser/App.java
@@ -1,7 +1,20 @@
 package Text_Compresser;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args){
-        System.out.print("Hi")
+        try (FileReader in = new FileReader("./Text_Compresser/input.txt");
+             FileWriter out = new FileWriter("output_java.sc")) {
+            int c;
+            while ((c = in.read()) != -1){
+                out.write(c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
