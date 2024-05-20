2707c5f08f59c1da7815f43a1fdbdc5d8a016cfb
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2024-05-20 13:46:10 -0400

    add compress function

--- Text_Compresser/App.java
+++ Text_Compresser/App.java
@@ -3,14 +3,31 @@ package Text_Compresser;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args){
         try (FileReader in = new FileReader("./Text_Compresser/input.txt");
              FileWriter out = new FileWriter("output_java.sc")) {
-            int c;
-            while ((c = in.read()) != -1){
-                out.write(c);
+            
+            Map<String, Integer> wordHashMap = new LinkedHashMap<>();
+            int index = 1;
+            int data;
+            StringBuilder wordBuilder = new StringBuilder();
+            while ((data = in.read()) != -1){
+                char c = (char) data;
+                if(Character.isWhitespace(c)){
+                    String word = wordBuilder.toString();
+                    wordBuilder.setLength(0);
+                    if(!word.isEmpty()){
+                        wordHashMap.putIfAbsent(word, index++);
+                        out.write(wordHashMap.get(word)+ " ");
+                    }
+                } else {
+                    wordBuilder.append(c); 
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
